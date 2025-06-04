1c168797e59e05a9e5e60320b4348d486463ffa3
onghyeya <hhw3960@gmail.com>
2025-06-04 17:39:51 +0900

    C#으로 프로그램 만들기

---
+++ study_c#/Hello/Hello/MainApp.cs
@@ -0,0 +1,19 @@
+﻿using System; // Systme 생략 가능
+using static System.Console; // System.Console 생략 가능
+
+namespace Hello
+{
+    internal class MainApp
+    {
+        // 프로그램 실행이 시작되는 곳
+        static void Main(string[] args) // Main 메서드가 종료 되면 프로그램도 같이 종료됨
+        {
+            if (args.Length == 0)
+            {
+                Console.WriteLine("사용법 : Hello.exe <이름>");
+                return;
+            }
+            WriteLine("Hello, {0}", args[0]); // Hello,World를 프롬프트에 출력
+        }
+    }
+}
